260385b0d93c741288379e9dfdae1e8da347a5b2
David Arcus <arcus.david@gmail.com>
2023-03-22 19:47:24 +1100

    show detected image labels being typed out 80s-style

---
+++ src/app/components/showImageLabels/showImageLabels.directive.js
@@ -0,0 +1,61 @@
+(function () {
+    'use strict';
+
+    angular
+        .module('evenstrangerthings')
+        .directive('showImageLabels', showImageLabels);
+
+    /** @ngInject */
+    function showImageLabels() {
+        var directive = {
+            restrict: 'E',
+            templateUrl: 'app/components/showImageLabels/showImageLabels.html',
+            scope: {
+                renderToCanvas: '=',
+                imageLabels: '='
+            },
+            controller: ShowImageLabelsController,
+            controllerAs: 'vm',
+            bindToController: true
+        };
+
+        return directive;
+
+        /** @ngInject */
+        function ShowImageLabelsController($window, $scope, $log) {
+
+            var vm = this;
+            var i = 0;
+            var speed = 50;
+            var typewriter = document.getElementById("typewriter");
+            var timeoutID;
+
+            $scope.$watch('vm.renderToCanvas', function (ready) {
+
+                i = 0;
+                vm.renderToCanvas = false;
+                typewriter.innerHTML = '';
+
+                if (ready) {
+
+                    var output = "I saw: " + vm.imageLabels.join(', ');
+
+                    function typeWriter() {
+
+                        if (i < output.length) {
+                            typewriter.innerHTML += output.charAt(i);
+                            i++;
+                            timeoutID = $window.setTimeout(typeWriter, speed);
+                        } else {
+                            $window.clearTimeout(timeoutID);
+                        }
+                    }
+
+                    typeWriter();
+
+                }
+            })
+        }
+    }
+
+})();
